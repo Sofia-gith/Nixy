b3606f00230b7a91c3fa9ecd4b44012cbfd28f7b
sofia <sofiafamorim@gmail.com>
2025-03-20 22:38:23 -0300

    add telas

--- api/index.js
+++ api/index.js
@@ -7,25 +7,50 @@ app.use(cors())
 
 app.set('view engine', 'ejs');
 app.set('views', './view');
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', 
-    { 
-        title: 'Página Inicial', 
-        message: 'Bem-vindo ao Express com EJS!' 
-    });
+const menuItems = [
+  { nome: "Agenda", link: "/agenda" },
+  { nome: "Minhas estatísticas", link: "/estatisticas" },
+  { nome: "Anotações", link: "/anotacoes" },
+  { nome: "Pomodoro", link: "/pomodoro" }
+];
+
+app.use((req, res, next) => {
+  res.locals.menuItems = menuItems;
+  next();
 });
 
+    app.get("/", (req, res) => { 
+      res.render("index", { usuarioNome: "usuario_nome", mostrarMenu: true });
+    });
+
+  app.get("/agenda", (req, res) => { 
+    res.render("agenda", { usuarioNome: "usuario_nome" }); 
+  });
+  
+  app.get("/estatisticas", (req, res) => { 
+    res.render("estatisticas", { usuarioNome: "usuario_nome", mostrarMenu: true });
+  });
+  
+  app.get("/anotacoes", (req, res) => { 
+    res.render("anotacoes", { usuarioNome: "usuario_nome", mostrarMenu: true });
+  });
+  
+  app.get("/pomodoro", (req, res) => { 
+    res.render("pomodoro", { usuarioNome: "usuario_nome", mostrarMenu: true }); 
+  });
+
 app.get('/login', (req, res) => {
-    res.status(200).render('login');
+    res.status(200).render('login', {mostrarMenu: false });
 });
 
 app.get('/cadastro', (req, res) => {
-    res.status(200).render('cadastro');
+    res.status(200).render('cadastro', {mostrarMenu: false });
 });
 
 app.get('/landingPage', (req, res) => {
-    res.status(200).render('landingPage');
+    res.status(200).render('landingPage', {mostrarMenu: false });
 });
 
 app.listen(8800, () => {
